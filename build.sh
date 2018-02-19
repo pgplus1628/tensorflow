59aeed5273b6f94a7f17d50d462c2bde307641ac
Pin Gao <pin.gao2008@gmail.com>
2018-02-19 16:12:08 +0000

    fix.

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-#bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package 
+#bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 #
 #echo "----------------- build OK ----------------"
 
-bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0"
 echo "----------------- build pip OK ----------------"
 
-pip uninstall tensorflow
-pip uninstall tensorflow
-
-pip install /tmp/tensorflow_pkg/tensorflow-1.4.0-cp27-cp27mu-linux_x86_64.whl
-echo "----------------- install pip OK ----------------"
+#pip uninstall tensorflow
+#pip uninstall tensorflow
+#
+#pip install /tmp/tensorflow_pkg/tensorflow-1.4.0-cp27-cp27mu-linux_x86_64.whl
+#echo "----------------- install pip OK ----------------"
